3a5127d774b4a234fa0063584a7d1e93e6a6e6e3
gamingchulitha1212-netizen <gamingchulitha1212@gmail.com>
2026-09-11 01:42:40 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase project: mr-gaming-c4s-website
+var firebaseConfig = {
+  apiKey: "AIzaSyCAR-h67q3M8-7cRB5mOcY9KqJYeShWonU",
+  authDomain: "mr-gaming-c4s-website.firebaseapp.com",
+  databaseURL: "https://mr-gaming-c4s-website-default-rtdb.firebaseio.com",
+  projectId: "mr-gaming-c4s-website",
+  storageBucket: "mr-gaming-c4s-website.firebasestorage.app",
+  messagingSenderId: "310015601996",
+  appId: "1:310015601996:web:b9f66181200090c5fc33e9",
+  measurementId: "G-E6Q8HCKVYM"
+};
